a00a9fabc74db067484c415e5332d08acc535823
kojoankomah <patrickankomah4@gmai;.com>
2025-02-14 18:29:44 -0800

    Completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new Square("Yellow", 8);
+        Rectangle rectangle1 = new Rectangle("Blue",8,4);
+        Circle circle1 = new Circle("Brown", 5);
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square1);
+        shapes.Add(circle1);
+        shapes.Add(rectangle1);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The Color is {color}\nThe area is {area}");
+        }
     }
 }
