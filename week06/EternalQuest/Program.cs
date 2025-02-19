c79466363c1252f1b918a221e65bfc913cab6a98
kojoankomah <patrickankomah4@gmai;.com>
2025-02-19 01:28:47 -0800

    Updated

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,6 +1,6 @@
 // To show creativity 
 // Instead of clearing the the console before displaying goals, I print a separator line for readability and also access to past result
-// I included a leveling feature based on points as a motivational addition. It encourages users to keep completing goals.
+// I included a levelling feature based on points as a motivational addition. It encourages users to keep completing goals.
 
 
 using System;
